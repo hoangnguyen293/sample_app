a2cf471d99d74829c72f16d9b85e1675e76bc4f2
Hoang Nguyen <nguyenvanhoang2931996@gmail.com>
2018-07-11 11:07:50 +0700

    Finish password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,11 @@
 class UserMailer < ApplicationMailer
   def account_activation user
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: t("account_activation.account")
+  end
+
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t("password_reset.reset_password")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,9 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2018_07_13_013906) do
+
+ActiveRecord::Schema.define(version: 2018_07_15_025901) do
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -21,6 +23,9 @@ ActiveRecord::Schema.define(version: 2018_07_13_013906) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
